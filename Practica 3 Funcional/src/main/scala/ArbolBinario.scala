91872d5dd037a9cd6fa5b6cb96d7b54d2b6f59fb
RocioL17 <rocio.learreta@uap.edu.ar>
2024-10-28 20:33:40 -0300

    binary tree activities completed

--- Practica 3 Funcional/src/main/scala/ArbolBinario.scala
+++ Practica 3 Funcional/src/main/scala/ArbolBinario.scala
@@ -30,8 +30,7 @@
      * con 5, como vemos que es menor que 5, nos vamos al sub-árbol de la derecha. Ahora estaríamos en 7,
      * como es menor que 8 continuaríamos hacia la derecha. De esta forma encontraríamos el elemento en
      * tres pasos. Si estuviéramos usando una lista (o un árbol no balanceado), nos hubiera costado unos 7
-     * pasos encontrar el 8.
-     *
+     * pasos encontrar el 8. 
      */
 
     sealed abstract class Tree
@@ -41,58 +40,118 @@
     /*
      * Inserta un elemento
      */
-    def insert(nro: Int, tree: Tree): Tree = {
-      if (nro > tree.nro) insert(nro, tree.right)
-      else if (nro < tree.nro) insert(nro, tree.left)
-      else tree.Node.nro
+    def insert(nro: Int, tree: Tree): Tree = tree match {
+      case EmptyTree() =>
+        Node(nro, EmptyTree(), EmptyTree()) // Crea un nuevo nodo con el valor en un árbol vacío
+
+      case Node(value, left, right) =>
+        if (nro < value) Node(value, insert(nro, left), right) // Inserta en el subárbol izquierdo
+        else if (nro > value) Node(value, left, insert(nro, right)) // Inserta en el subárbol derecho
+        else tree // Si el valor ya existe, devuelve el árbol sin cambios
     }
 
     /**
      * Devuelve verdadero si el elemento esta en el arbol.
      */
-    def elem(nro: Int, tree: Tree): Boolean = ???
+    def elem(nro: Int, tree: Tree): Boolean = tree match {
+      case EmptyTree() => false
+      // case Node(nro, left, right) => true // si tenemos un subárbol con el número que le pasamos
+      case Node(numero, left, right) => 
+        if (numero == nro) true
+        else elem(nro, left) || elem(nro, right)
+    }
 
     /**
      * Devuelve la cantidad de nodos del arbol
      */
-    def countNodes(tree: Tree): Int = ???
+    def countNodes(tree: Tree): Int = tree match {
+      case EmptyTree() => 0 // Si el árbol está vacío, devuelve
+      case Node(_, left, right) => 1 + countNodes(left) + countNodes(right)
+    }
 
     /**
      * Devuelve un arbol donde se aplico esta funcion
      */
-    def apply(tree: Tree, fx: (Int) => Int): Tree = ???
+    def apply(tree: Tree, fx: (Int) => Int): Tree = tree match {
+      case EmptyTree() => tree
+      case Node(nro, left, right) => 
+        Node(fx(nro), apply(left, fx), apply(right, fx)) //aplico la búsqueda en ambos sub árboles
+    }
 
     /**
      * Devuelve un arbol con el doble de los elementos
      */
-    def double(tree: Tree): Tree = ???
+    def double(tree: Tree): Tree = tree match {
+      case EmptyTree() => tree
+      case Node(nro, left, right) => 
+        // Node(nro * 2, double(left), double(right))
+        apply(tree, _*2)
+    }
 
     /**
      * Devuelve una lista ordenada
      */
-    def toList(tree: Tree): List[Int] = ???
+    def toList(tree: Tree): List[Int] = tree match {
+      case EmptyTree() => Nil
+      case Node(nro, left, right) => 
+        toList(left) ++ List(nro) ++ toList(right) //hago una unión entre la lista hacia la izquierda y hacia la derecha
+    }
 
     /**
      * Retorna el nivel del arbol
      */
-    def nivel(ts: Tree): Int = ???
+    def nivel(ts: Tree): Int = ts match {
+      case EmptyTree() => 0
+      case Node(nro, left, right) => 
+        1 + Math.max(nivel(left), nivel(right))
+    }
 
 
     /**
-     * Balancea el arbol si no esta balanceado
+     * Balancea el árbol si no está balanceado.
+     * Creo un árbol nuevo, ordenándolo siempre.
      */
-    def balancear(tree: Tree): Tree = ???
+    def balancear(tree: Tree): Tree = {
+      // Convertir el árbol a una lista ordenada
+      val lista = toList(tree)
+      
+      // Si el árbol ya está balanceado, lo devolvemos sin cambios
+      if (isBal(tree)) tree
+      else listToBalancedTree(lista) // Construimos un nuevo árbol balanceado desde la lista
+    }
 
     /**
-     * Retorna true si el arbol esta balanceado
-     * (nivel(right) - nivel(left)) <= 1
+     * Retorna true si el árbol está balanceado:
+     * la diferencia de niveles entre los subárboles no supera 1.
      */
-    def isBal(tree: Tree) = ???
+    def isBal(tree: Tree): Boolean = tree match {
+      case EmptyTree() => true
+      case Node(_, left, right) =>
+        // Math.abs(nivel(left) - nivel(right)) <= 1 && isBal(left) && isBal(right)
+        if (nivel(left) == nivel(right)) true
+        else false
+    }
+
+    /**
+     * FUNCION EXTRA PARA QUE PUEDA CONVERTIR DE UNA LISTA A UN ÁRBOL
+     */
+    def listToBalancedTree(listaOrdenada: List[Int]): Tree = {
+      if (listaOrdenada.isEmpty) EmptyTree()
+      else {
+        val middle = listaOrdenada.length / 2
+        Node(listaOrdenada(middle), listToBalancedTree(listaOrdenada.take(middle)), listToBalancedTree(listaOrdenada.drop(middle + 1))
+        )
+      }
+    }
 
     /**
      * Devuelve una lista ordenada en formato string
      */
-    def toString(tree: Tree): String = ???
+    def toString(tree: Tree): String = tree match {
+      case EmptyTree() => ""
+      case Node(nro, left, right) =>
+        toString(left) + "," + nro + "," + toString(right)
+    }
 
 
   }
